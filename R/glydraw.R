2fa331cfd4cb883503d4d66d719017b5367f28ae
Xisong-Feng <1225774778@qq.com>
2025-11-04 20:53:38 +0800

    glydraw.R

--- R/glydraw.R
+++ R/glydraw.R
@@ -1,3 +1,106 @@
+# glycan mapping
+glycan_color = c(
+  'glyWhite'='#FFFFFF',
+  'glyBlue'='#0385AE', 'glyGreen'='#058F60',
+  'glyYellow'='#FCC326','glyOrange'='#EF6130',
+  'glyPink'='#F39EA0', 'glyPurple'='#A15989',
+  'glyLightBlue'='#91D3E3', 'glyBrown'='#9F6D55',
+  'glyRed'='#C23537'
+)
+
+glycan_dict <- list(
+  'Hex'=c('Hex','glyWhite'),
+  'Glc'=c('Hex','glyBlue'),'Man'=c('Hex','glyGreen'),
+  'Gal'=c('Hex','glyYellow'),'Gul'=c('Hex','glyOrange'),
+  'Alt'=c('Hex','glyPink'),'All'=c('Hex','glyPurple'),
+  'Tal'=c('Hex','glyLightBlue'),'Ido'=c('Hex','glyBrown'),
+
+  'HexNAc'=c('HexNAc','glyWhite'),
+  'GlcNAc'=c('HexNAc','glyBlue'),'ManNAc'=c('HexNAc','glyGreen'),
+  'GalNAc'=c('HexNAc','glyYellow'),'GulNAc'=c('HexNAc','glyOrange'),
+  'AltNAc'=c('HexNAc','glyPink'),'AllNAc'=c('HexNAc','glyPurple'),
+  'TalNAc'=c('HexNAc','glyLightBlue'),'IdoNAc'=c('HexNAc','glyBrown'),
+
+  'HexN'=c('HexN','glyWhite','glyWhite'),
+  'GlcN'=c('HexN','glyBlue','glyWhite'),'ManN'=c('HexN','glyGreen','glyWhite'),
+  'GalN'=c('HexN','glyYellow','glyWhite'),'GulN'=c('HexN','glyOrange','glyWhite'),
+  'AltN'=c('HexN','glyPink','glyWhite'),'AllN'=c('HexN','glyPurple','glyWhite'),
+  'TalN'=c('HexN','glyLightBlue','glyWhite'),'IdoN'=c('HexN','glyBrown','glyWhite'),
+
+  'HexA'=c('HexA','glyWhite','glyWhite'),
+  'GlcA'=c('HexA','glyBlue','glyWhite'),'ManA'=c('HexA','glyGreen','glyWhite'),
+  'GalA'=c('HexA','glyYellow','glyWhite'),'GulA'=c('HexA','glyOrange','glyWhite'),
+  'AltA'=c('HexA','glyPink','glyWhite'),'AllA'=c('HexA','glyPurple','glyWhite'),
+  'TalA'=c('HexA','glyLightBlue','glyWhite'),'IdoA'=c('HexA','glyBrown','glyWhite'),
+
+  'dHex'=c('dHex','glyWhite'),
+  'Qui'=c('dHex','glyBlue'),'Rha'=c('dHex','glyGreen'),
+  '6dGul'=c('dHex','glyOrange'),'6dAlt'=c('dHex','glyPink'),
+  '6dTal'=c('dHex','glyLightBlue'),'Fuc'=c('dHex','glyRed'),
+
+  'dHexNAc'=c('dHexNAc','glyWhite','glyWhite'),
+  'QuiNAc'=c('dHexNAc','glyBlue','glyWhite'),'RhaNAc'=c('dHexNAc','glyGreen','glyWhite'),
+  '6dAltNAc'=c('dHexNAc','glyPink','glyWhite'),'6dTalNAc'=c('dHexNAc','glyLightBlue','glyWhite'),
+  'FucNAc'=c('dHexNAc','glyRed','glyWhite'),
+
+  'Pen'=c('Pen','glyWhite'),
+  'Ara'=c('Pen','glyGreen'),'Lyx'=c('Pne','glyYellow'),
+  'Xyl'=c('Pen','glyOrange'),'Rib'=c('Pen','glyPink'),
+
+  'dNon'=c('dNon','glyWhite'),
+  'Kdn'=c('dNon','glyGreen'),'Neu5Ac'=c('dNon','glyPurple'),
+  'Neu5Gc'=c('dNon','glyLightBlue'),'Neu'=c('dNon','glyBrown'),
+  'Sia'=c('dNon','glyRed'),
+
+  'ddNon'=c('ddNon','glyWhite'),
+  'Pse'=c('ddNon','glyGreen'),'Leg'=c('ddNon','glyYellow'),
+  'Aci'=c('ddNon','glyPink'),'4eLeg'=c('ddNon','glyLightBlue'),
+
+  'UnKnown'=c('UnKnown','glyWhite'),
+  'Bac'=c('UnKnown','glyBlue'),'LDmanHep'=c('UnKnown','glyGreen'),
+  'Kdo'=c('UnKnown','glyYellow'),'Dha'=c('UnKnown','glyOrange'),
+  'DDmanHep'=c('UnKnown','glyPink'),'MurNAc'=c('UnKnown','glyPurple'),
+  'MurNGc'=c('UnKnown','glyLightBlue'),'Mur'=c('UnKnown','glyBrown'),
+
+  'Assigned'=c('Assigned','glyWhite'),
+  'Api'=c('Assigned','glyBlue'),'Fru'=c('Assigned','glyGreen'),
+  'Tag'=c('Assigned','glyYellow'),'Sor'=c('Assigned','glyOrange'),
+  'Psi'=c('Assigned','glyPink')
+)
+
+glycan_shape <- list(
+  'Hex' = data.frame(x=c(-1,-1,1,1,-1), # The center is the Core of shape
+                     y=c(-1,1,1,-1,-1)),
+  'HexNAc' = data.frame(x=c(-1,-1,1,1,-1),
+                        y=c(-1,1,1,-1,-1)),
+  'HexN' = data.frame(x=c(-1,1,1,-1),
+                      y=c(1,1,-1,1),
+                      xx=c(-1,1,-1,-1),
+                      yy=c(1,-1,-1,1)),
+  'HexA' = data.frame(x=c(-1,0,1,-1),
+                      y=c(0,1,0,0),
+                      xx=c(1,0,-1,1),
+                      yy=c(0,-1,0,0)),
+  'dHex' = data.frame(x=c(-1,0,1,-1),
+                      y=c(0,sqrt(3),0,0)), # The center is the Midpoint of the Base
+  'dHexNAc' = data.frame(x=c(0,1,0,0),
+                         y=c(0.67*sqrt(3),-0.33*sqrt(3),-0.33*sqrt(3),0.67*sqrt(3)),
+                         xx=c(0,-1,0,0),
+                         yy=c(0.67*sqrt(3),-0.33*sqrt(3),-0.33*sqrt(3),0.67*sqrt(3))),
+  'ddHex' = data.frame(x=c(-1,1,1,-1),
+                       y=c(0.5,0.5,-0.5,-0.5)),
+  'Pen' = data.frame(x=c(0,0.2345,0.9516,0.3798,0.5872,0,-0.5872,-0.3798,-0.9516,-0.2345,0),
+                     y=c(0.905,0.2289,0.2132,-0.219,-0.905,-0.4961,-0.905,-0.219,0.2132,0.2289,0.905)),
+  'dNon' = data.frame(x=c(0,1,0,-1,0),
+                      y=c(1,0,-1,0,1)),
+  'ddNon' = data.frame(x=c(0,1.2,0,-1.2,0),
+                       y=c(0.8,0,-0.8,0,0.8)),
+  'UnKnown' = data.frame(x=c(0.6,1,0.6,-0.6,-1,-0.6,0.6),
+                         y=c(0.6,0,-0.6,-0.6,0,0.6,0.6)),
+  'Assigned' = data.frame(x=c(0,0.9516,0.5872,-0.5872,-0.9516,0),
+                          y=c(0.905,0.2132,-0.905,-0.905,0.2132,0.905))
+)
+
 #' Title Initialize Vertices Coordinate
 #'
 #' @param structure igraph
@@ -487,16 +590,33 @@ gly_annotation <- function(structure,coor){
 gly_draw <- function(structure){
   coor <- coor_cal(structure)
   gly_list <- data.frame(coor,'glycoform' = glycoform_info(structure))
-  struc_annotation <- gly_annotation(structure,coor)
-  # 创建颜色和形状的映射
-  color <- c('GlcNAc'= '#3300CC','Man'='#339933','Gal'='#FFCC33','Fuc'='#CC0000','Fuc_down'='#CC0000','GalNAc'='#FFCC33','Neu5Ac'='#FF00FF')
-  shape <- c('GlcNAc'= 22,'Man'=21,'Gal'=21,'Fuc'=25,'Fuc_down'=24,'GalNAc'=22,'Neu5Ac'=23)
+  # Rename colnames of gly_list
+  colnames(gly_list) <- c('center_x','center_y','glycoform')
+  # Draw Glycan Shape, where gly_list contains center_x, center_y, glycoform 3 columns
+  polygon_coor <- gly_list |>
+    purrr::pmap_dfr(function(center_x, center_y, glycoform){
+      composition <- glycan_dict[[glycoform]][1] # Mapping the Composition of Glycoform, e.g.'Fuc'->'dHex'
+      df1 <- data.frame(
+        point_x = c(0.1*glycan_shape[[composition]]$x + center_x),
+        point_y = c(0.1*glycan_shape[[composition]]$y + center_y),
+        # For Distinguishing the Coordinates of each point
+        group = paste0(glycoform, center_x, "_", center_y),
+        color = glycan_dict[[glycoform]][2])
+      if (length(glycan_dict[[glycoform]]) > 2){
+        df2 <- data.frame(
+          point_x = c(0.1*glycan_shape[[composition]]$xx + center_x),
+          point_y = c(0.1*glycan_shape[[composition]]$yy + center_y),
+          # For Distinguishing the Coordinates of each point
+          group = paste0(glycoform, center_x, "_", center_y,'remain'),
+          color = glycan_dict[[glycoform]][3])
+        df1 <- dplyr::bind_rows(df1,df2)
+      }
+      return(df1)
+    })
 
-  # 匹配顶点的颜色和形状
-  gly_color <- color[as.character(gly_list$glycoform)]
-  names(gly_color) = NULL
-  gly_shape <- shape[as.character(gly_list$glycoform)]
-  names(gly_shape) = NULL
+  filled_color <- glycan_color[as.character(polygon_coor$color)]
+
+  struc_annotation <- gly_annotation(structure,coor)
 
   # connect information
   gly_connect <- connect_info(structure, coor)
@@ -513,8 +633,9 @@ gly_draw <- function(structure){
                           ggplot2::aes(x = start_x, y = start_y,
                                        xend = end_x, yend = end_y),
                           linewidth = 1.5)+
-    ggplot2::geom_point(ggplot2::aes(x=gly_list[,'x'],y=gly_list[,'y']),
-                        shape=gly_shape,fill=gly_color,size=12,color='black',stroke=0.5)+
+    ggplot2::geom_polygon(data = polygon_coor,
+                          ggplot2::aes(x = point_x, y = point_y, group = group),
+                          fill=filled_color,color='black')+
     ggplot2::geom_text(data = struc_annotation,
                        ggplot2::aes(x = x, y = y, label = annot),
                        family = 'Arial',
