e516472f82ae295b4ea4296913351184cb343c3c
Xisong-Feng <1225774778@qq.com>
2026-01-19 17:01:01 +0800

    fix: logical problem of numbers judgement
    It should be out_degree(structure,ver)[1] >= 2 when calling offset_chil_coor()

--- R/glydraw.R
+++ R/glydraw.R
@@ -355,7 +355,7 @@ process_two_neighbors <- function(coor,structure,ver){
     coor <- offset_chil_coor(structure,arrange_neigh_pos[1],coor,0.5)
     coor <- offset_chil_coor(structure,arrange_neigh_pos[2],coor,-0.5)
   }
-  else if(out_degree(structure,ver)[1] >= 1){ # More than 1 branch along the path
+  else if(out_degree(structure,ver)[1] >= 2){ # More than 1 branch along the path
     coor <- offset_chil_coor(structure,arrange_neigh_pos[1],coor,0.5)
     coor <- offset_chil_coor(structure,arrange_neigh_pos[2],coor,-0.5)
     coor <- process_multiple_branches(coor,structure,ver)
