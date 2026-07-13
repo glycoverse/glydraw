32b2569c5de488d92a651cda7fadeb28b67a58e2
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-07-13 21:05:28 +0800

    feat: add glycan axis scale functions

---
+++ tests/testthat/test-scale-glycan.R
@@ -0,0 +1,190 @@
+.axis_glycan_labels <- function(plot, axis_name) {
+  plot_grob <- ggplot2::ggplotGrob(plot)
+  axis_grob <- plot_grob$grobs[[match(axis_name, plot_grob$layout$name)]]
+  labels <- purrr::keep(
+    axis_grob$children$axis$grobs,
+    inherits,
+    what = "glycan_axis_labels"
+  )
+
+  labels[[1]]
+}
+
+test_that("scale_x_glycan draws vertical cartoon labels", {
+  data <- data.frame(
+    structure = c(
+      "Gal(b1-3)GalNAc(a1-",
+      "Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-"
+    ),
+    value = c(1, 2)
+  )
+
+  plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$structure, y = .data$value)
+  ) +
+    ggplot2::geom_col() +
+    scale_x_glycan()
+  labels <- .axis_glycan_labels(plot, "axis-b")
+
+  expect_s3_class(labels, "glycan_axis_labels")
+  expect_length(labels$children, nrow(data))
+  purrr::walk(labels$children, expect_s3_class, "glycanGrob")
+  expect_true(all(purrr::map_lgl(
+    labels$children,
+    "glydraw_axis_vertical"
+  )))
+  expect_equal(
+    unname(purrr::map_dbl(labels$children, "glydraw_vjust")),
+    c(0, 0)
+  )
+  expect_no_error(ggplot2::ggplotGrob(plot))
+})
+
+test_that("scale_y_glycan draws horizontal cartoon labels", {
+  data <- data.frame(
+    structure = c(
+      "Gal(b1-3)GalNAc(a1-",
+      "Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-"
+    ),
+    value = c(1, 2)
+  )
+
+  plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$value, y = .data$structure)
+  ) +
+    ggplot2::geom_col() +
+    scale_y_glycan()
+  labels <- .axis_glycan_labels(plot, "axis-l")
+
+  expect_s3_class(labels, "glycan_axis_labels")
+  expect_length(labels$children, nrow(data))
+  purrr::walk(labels$children, expect_s3_class, "glycanGrob")
+  expect_false(any(purrr::map_lgl(
+    labels$children,
+    "glydraw_axis_vertical"
+  )))
+  expect_equal(
+    unname(purrr::map_dbl(labels$children, "glydraw_hjust")),
+    c(1, 1)
+  )
+  expect_no_error(ggplot2::ggplotGrob(plot))
+})
+
+test_that("glycan axis alignment can be adjusted", {
+  data <- data.frame(
+    structure = "Gal(b1-3)GalNAc(a1-",
+    value = 1
+  )
+  x_plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$structure, y = .data$value)
+  ) +
+    ggplot2::geom_col() +
+    scale_x_glycan(vjust = 1)
+  y_plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$value, y = .data$structure)
+  ) +
+    ggplot2::geom_col() +
+    scale_y_glycan(hjust = 0)
+  x_label <- .axis_glycan_labels(x_plot, "axis-b")$children[[1]]
+  y_label <- .axis_glycan_labels(y_plot, "axis-l")$children[[1]]
+
+  expect_equal(x_label$glydraw_vjust, 1)
+  expect_equal(y_label$glydraw_hjust, 0)
+})
+
+test_that("glycan axis labels can be nudged", {
+  data <- data.frame(
+    structure = "Gal(b1-3)GalNAc(a1-",
+    value = 1
+  )
+  x_plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$structure, y = .data$value)
+  ) +
+    ggplot2::geom_col() +
+    scale_x_glycan(nudge_x = 1, nudge_y = -2)
+  y_plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$value, y = .data$structure)
+  ) +
+    ggplot2::geom_col() +
+    scale_y_glycan(nudge_x = -3, nudge_y = 4)
+  x_label <- .axis_glycan_labels(x_plot, "axis-b")$children[[1]]
+  y_label <- .axis_glycan_labels(y_plot, "axis-l")$children[[1]]
+
+  expect_equal(x_label$glydraw_nudge_x, 1)
+  expect_equal(x_label$glydraw_nudge_y, -2)
+  expect_equal(y_label$glydraw_nudge_x, -3)
+  expect_equal(y_label$glydraw_nudge_y, 4)
+})
+
+test_that("glycan axis labels support reducing-end annotations", {
+  data <- data.frame(
+    structure = "Gal(b1-3)GalNAc(a1-",
+    value = 1
+  )
+  x_plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$structure, y = .data$value)
+  ) +
+    ggplot2::geom_col() +
+    scale_x_glycan(red_end = "~")
+  y_plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$value, y = .data$structure)
+  ) +
+    ggplot2::geom_col() +
+    scale_y_glycan(red_end = "Reducing end")
+  x_label <- .axis_glycan_labels(x_plot, "axis-b")$children[[1]]
+  y_label <- .axis_glycan_labels(y_plot, "axis-l")$children[[1]]
+
+  expect_gt(nrow(x_label$annotation_data$reducing_info$wave), 0)
+  expect_true(any(
+    y_label$annotation_data$reducing_info$annotation$is_red_end_text
+  ))
+  expect_match(
+    y_label$annotation_data$reducing_info$annotation$annot[[2]],
+    "Reducing end"
+  )
+})
+
+test_that("glycan axis scales use plain cartoon parameters", {
+  data <- data.frame(
+    structure = "Gal(b1-3)GalNAc(a1-",
+    value = 1
+  )
+  plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$structure, y = .data$value)
+  ) +
+    ggplot2::geom_col() +
+    scale_x_glycan(size = 0.6, show_linkage = TRUE)
+  label <- .axis_glycan_labels(plot, "axis-b")$children[[1]]
+
+  expect_false("guide" %in% names(formals(scale_x_glycan)))
+  expect_false("guide" %in% names(formals(scale_y_glycan)))
+  expect_equal(label$glydraw_scale, 0.6)
+  expect_true(label$show_linkage)
+})
+
+test_that("scale_x_glycan renders cartoon labels", {
+  data <- data.frame(
+    structure = c(
+      "Gal(b1-3)GalNAc(a1-",
+      "Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-"
+    ),
+    value = c(1, 2)
+  )
+  plot <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = .data$structure, y = .data$value)
+  ) +
+    ggplot2::geom_col() +
+    scale_x_glycan()
+
+  vdiffr::expect_doppelganger("glycan x-axis labels", plot)
+})
